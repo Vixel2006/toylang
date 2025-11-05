666e7d23c64182a4f6e5fcd96fd6be11576720c2
Vixel2006 <yusufshihata2006@gmail.com>
2025-11-05 16:20:44 +0200

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/LLVMContext.h"
+#include "llvm/IR/Module.h"
+#include "llvm/IR/Verifier.h"
+#include "llvm/Support/raw_ostream.h"
+
+using namespace llvm;
+
+int main() {
+    return 0;
+}
